d038f3ed6c8fe8dca1a58aef0ce9e3ad6d6b94b7
bachhungcb <bachhungcb@gmail.com>
2024-04-08 12:53:27 +0700

    doing some query for database

---
+++ sql/Querying_1.sql
@@ -0,0 +1,56 @@
+﻿--Đưa ra các thông tin về một bộ anime với anime id cho trước--
+SELECT	anime.anime_id, anime.title, informations.scores, informations.ranks, characters.names, characters.roles,
+		anime_status.aired_from,anime_status.aired_to
+FROM anime
+ JOIN characters ON characters.anime_id = anime.anime_id
+ JOIN informations ON informations.anime_id = anime.anime_id
+ JOIN anime_status ON anime_status.anime_id = anime.anime_id
+WHERE anime.anime_id = 0;
+
+
+--Chọn ra top 3 anime có điểm cao nhất từ dưới lên--
+SELECT TOP 3 anime.anime_id, anime.title, anime.synopsis, anime.genres, informations.scores
+FROM anime
+JOIN informations ON informations.anime_id = anime.anime_id
+ORDER BY informations.scores;
+
+--chọn ra top 3 anime có điểm cao nhất từ trên xuống--
+SELECT TOP 3 anime.anime_id, anime.title, anime.synopsis, anime.genres, informations.scores
+FROM anime
+JOIN informations ON informations.anime_id = anime.anime_id
+ORDER BY informations.scores DESC;
+
+--Đưa ra tên của top 3 studio có điểm trung bình cao nhất--
+SELECT TOP 3  producers.producers_name, AVG(CONVERT(DECIMAL,informations.scores)) AS Score FROM informations
+JOIN anime ON anime.anime_id = informations.anime_id
+JOIN anime_producers ON anime_producers.anime_id = anime.anime_id
+JOIN producers ON producers.producers_id = anime_producers.producers_id
+GROUP BY producers.producers_name
+ORDER BY AVG(CONVERT(DECIMAL,informations.scores)) DESC;
+
+--Đưa ra điểm trung bình của một studio anime--
+SELECT AVG(CONVERT(DECIMAL,informations.scores)) AS Score, producers.producers_name FROM informations
+JOIN anime ON anime.anime_id = informations.anime_id
+JOIN anime_producers ON anime_producers.anime_id = anime.anime_id
+JOIN producers ON producers.producers_id = anime_producers.producers_id
+WHERE producers.producers_id = 0
+GROUP BY producers.producers_name;
+
+--Đưa ra toàn bộ điểm trung bình của các studio anime--
+SELECT AVG(CONVERT(DECIMAL,informations.scores)) AS Score, producers.producers_name FROM informations
+JOIN anime ON anime.anime_id = informations.anime_id
+JOIN anime_producers ON anime_producers.anime_id = anime.anime_id
+JOIN producers ON producers.producers_id = anime_producers.producers_id
+GROUP BY producers.producers_name
+ORDER BY AVG(CONVERT(DECIMAL,informations.scores)) DESC;
+
+--Đưa ra 3 anime có điểm thấp nhất--
+SELECT TOP 3 anime.title, informations.scores FROM anime
+JOIN informations ON anime.anime_id = informations.anime_id
+ORDER BY informations.scores;
+
+--Đưa ra anime có thể loại X--
+SELECT anime.title, anime.genres FROM anime
+WHERE anime.genres LIKE '%Historical%';
+
+
