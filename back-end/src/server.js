7d2b7a7dc652107d06f3f3baa6f4e7857c346ec3
Phuong Do Manh <phuong.dm225660@sis.hust.edu.vn>
2024-04-09 23:11:54 +0700

    connect fe to be

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,43 +1,14 @@
+require('dotenv').config();
 const express = require('express');
-const app = express();
-
+const cors = require('cors');
 const port = process.env.PORT || 3030;//8080
 const hostname = process.env.DB_HOST || 'localhost';
 
-//biểu diễn cách connect tới database
-app.get('/', function (req, res) {
-   
-    var sql = require("mssql/msnodesqlv8");
-
-    // config for your database
-    var config = {
-        user: 'fuon1412',
-        password: 'phuongct1412',
-        server: 'localhost', 
-        database: 'AnimeDatabase',
-        port: 1433
-    };
-
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from anime', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
-});
-
+const app = express();
+app.use(cors());
+app.get("/api/v1", (req, res) => {
+    res.send("hello !!!!");
+  });
 app.listen(port, hostname, () => {
     console.log(`Server is running on http://${hostname}:${port}`);
 });
---
+++ front-end/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState();
+  const urlWithProxy = "/api/v1";
+
+  function getDataFromServer() {
+    axios
+      .get(urlWithProxy)
+      .then((res) => setData(res.data))
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  return (
+    <div className="App">
+      <button onClick={getDataFromServer}>Access server using proxy</button>
+      <p>data : {data}</p>
+    </div>
+  );
+}
+export default App
