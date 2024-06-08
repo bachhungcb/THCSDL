e2b281e1e4e17c5dc4e0ed459e9f4ee9b30e810b
bachhungcb <bachhungcb@gmail.com>
2024-06-08 16:18:46 +0700

    split procedure into smaller function

--- back-end/src/services/Service.js
+++ back-end/src/services/Service.js
@@ -1,5 +1,5 @@
 const createPool = require(`../config/database`);
-const sql = require('mssql')
+const sql = require("mssql");
 
 // Common function to execute queries
 const executeQuery = async (query, params) => {
@@ -17,6 +17,22 @@ const executeQuery = async (query, params) => {
   }
 };
 
+//Common function to execute stored procedures
+const executeProcedure = async (procedure, params) => {
+  try {
+    const pool = await createPool;
+    const request = pool.request();
+    for (const param of params) {
+      request.input(param.name, param.value);
+    }
+    const result = await request.execute(procedure);
+    return result.recordset;
+  } catch (err) {
+    console.error("Database query error:", error);
+    throw new DatabaseQueryError(error, query);
+  }
+};
+
 // Custom error class
 class DatabaseQueryError extends Error {
   constructor(originalError, query) {
@@ -27,7 +43,6 @@ class DatabaseQueryError extends Error {
 
 //get animes for homepage, display 50 animes per page
 const getDataForHomepage = async (offset) => {
-
   const query = `SELECT TOP (50) a.*, s.stat, i.scores, i.ranks, i.favourite, i.popularity, s.aired_from, s.aired_to, s.premiered
   FROM anime a
   LEFT JOIN anime_status s ON a.anime_id = s.anime_id
@@ -38,25 +53,16 @@ const getDataForHomepage = async (offset) => {
       ORDER BY anime_id
   )
   ORDER BY a.anime_id;`;
-  return executeQuery(query, [{ name: 'offset', value: offset }]);
+  return executeQuery(query, [{ name: "offset", value: offset }]);
 };
 
 const getAnimeById = async (animeId) => {
-try{
-  let pool = await createPool;
-  let result = 
-  await pool.request()
-            .input('anime_id', sql.Int, animeId)
-            .execute('AnimeInformation');
-  return result.recordset;
-}
-catch(err){
-  console.log(err);
-}
+  const procedure = "AnimeInformation";
+  const params = [{ name: "anime_id", value: animeId }];
+  return executeProcedure(procedure, params);
 };
 
 const getAnimeByType = async (offset, animeType) => {
-
   const query = `SELECT TOP (50) a.*, s.stat, i.scores, i.ranks, i.favourite, i.popularity, s.aired_from, s.aired_to, s.premiered
                 FROM anime a
                 LEFT JOIN anime_status s ON a.anime_id = s.anime_id
@@ -67,65 +73,62 @@ const getAnimeByType = async (offset, animeType) => {
                 ORDER BY anime_id
                 )
                 AND a.anime_type = @animeType
-                ORDER BY a.anime_id;`
-  return executeQuery(query, [{ name: 'offset', value: parseInt(offset)},
-                              { name: 'animeType', value: animeType}])
+                ORDER BY a.anime_id;`;
+  return executeQuery(query, [
+    { name: "offset", value: parseInt(offset) },
+    { name: "animeType", value: animeType },
+  ]);
 };
 
 const getCharacterByAnimeId = async (animeId) => {
-
   const query = `SELECT * 
                 FROM new_character 
                 JOIN link_character ON link_character.character_id = new_character.Id
-                WHERE link_character.anime_id = @anime_id`; 
-  return executeQuery(query, [{ name: 'anime_id', value: animeId }]);
+                WHERE link_character.anime_id = @anime_id`;
+  return executeQuery(query, [{ name: "anime_id", value: animeId }]);
 };
 
 const getProducerByAnimeId = async (animeId) => {
-  
   const query = `SELECT producers.producers_id AS Id, producers.producers_name AS producers 
                 FROM producers 
                 JOIN anime_producers ON anime_producers.producers_id = producers.producers_id 
                 JOIN anime ON anime.anime_id = anime_producers.anime_id 
-                WHERE anime.anime_id = @anime_id`
-  return executeQuery(query, [{ name: 'anime_id', value: animeId }]);
+                WHERE anime.anime_id = @anime_id`;
+  return executeQuery(query, [{ name: "anime_id", value: animeId }]);
 };
 
 const getAnimeByGenres = async (anime_genres) => {
-
   const query = `SELECT DISTINCT TOP 10 anime.title, genres.genres, informations.scores
                 FROM anime 
                 JOIN informations ON informations.anime_id = anime.anime_id 
                 JOIN link_genres ON link_genres.anime_id = anime.anime_id
                 JOIN genres ON genres.genres_id = link_genres.genres_id
                 WHERE genres.genres LIKE '%' + @anime_genres + '%' 
-                ORDER BY informations.scores DESC;`
-  return executeQuery(query, [{ name: 'anime_genres', value: anime_genres }]);
+                ORDER BY informations.scores DESC;`;
+  return executeQuery(query, [{ name: "anime_genres", value: anime_genres }]);
 };
 
 const getAnimeByName = async (anime_name) => {
-
   const query = `SELECT anime.title, informations.scores, informations.ranks, anime.episodes, anime.synopsis, 
                 anime_status.aired_from, anime_status.aired_to, informations.favourite, informations.popularity
                 FROM anime WITH (INDEX(idx_title))
                 INNER JOIN informations ON informations.anime_id = anime.anime_id
                 INNER JOIN anime_status ON anime_status.anime_id = anime.anime_id
-                WHERE anime.title LIKE '%'+@anime_name+'%'`
-  return executeQuery(query, [{ name: 'anime_name', value: anime_name }]);
+                WHERE anime.title LIKE '%'+@anime_name+'%'`;
+  return executeQuery(query, [{ name: "anime_name", value: anime_name }]);
 };
 
-
 const getCharacterByName = async (character_name) => {
-
   const query = `SELECT DISTINCT TOP 10 new_character.Name, new_character.Profile
   FROM new_character  
   WHERE new_character.Name LIKE '%' + @character_name + '%'
-  ORDER BY new_character.Name;`
-  return executeQuery(query, [{ name: 'character_name', value: character_name }]);
+  ORDER BY new_character.Name;`;
+  return executeQuery(query, [
+    { name: "character_name", value: character_name },
+  ]);
 };
 
 const getDataCharacterPage = async (offset) => {
-
   const query = `SELECT TOP 50 new_character.Name, new_character.Profile
   FROM new_character
   WHERE new_character.Id NOT IN (
@@ -133,32 +136,30 @@ const getDataCharacterPage = async (offset) => {
       FROM new_character 
       ORDER BY Id
   )
-  ORDER BY new_character.Name;`
-  return executeQuery(query, [{ name: 'offset', value: offset }]);
+  ORDER BY new_character.Name;`;
+  return executeQuery(query, [{ name: "offset", value: offset }]);
 };
 
-
 const getProducerByName = async (producers_name) => {
-
   const query = `SELECT producers.producers_name, producers.producers_id
   FROM producers
   WHERE producers.producers_name LIKE '%'+@producers_name+'%'
   ORDER BY producers.producers_id;`;
-  return executeQuery(query, [{ name: 'producers_name', value: producers_name }]);
+  return executeQuery(query, [
+    { name: "producers_name", value: producers_name },
+  ]);
 };
 
 const getGenresByAnimeId = async (animeId) => {
-
-  const query =  `SELECT genres.genres_id, genres.genres
+  const query = `SELECT genres.genres_id, genres.genres
   FROM genres
   JOIN link_genres ON link_genres.genres_id = genres.genres_id
   JOIN anime ON anime.anime_id = link_genres.anime_id
-  WHERE anime.anime_id = @anime_id`
-  return executeQuery(query, [{ name: 'anime_id', value: animeId }]);
+  WHERE anime.anime_id = @anime_id`;
+  return executeQuery(query, [{ name: "anime_id", value: animeId }]);
 };
 
 const getNumberOfAnime = async (offset = 0) => {
-
   const query = `SELECT producers.producers_name, SUM(anime.anime_id) AS Total_Anime 
   FROM informations
   JOIN anime ON anime.anime_id = informations.anime_id
@@ -167,17 +168,15 @@ const getNumberOfAnime = async (offset = 0) => {
   GROUP BY producers.producers_name
   ORDER BY AVG(CONVERT(DECIMAL,informations.scores)) DESC
   OFFSET (@offset) ROWS
-  FETCH NEXT 50 ROWS ONLY;`
-  return executeQuery(query, [{ name: 'offset', value: offset }]);
-
+  FETCH NEXT 50 ROWS ONLY;`;
+  return executeQuery(query, [{ name: "offset", value: offset }]);
 };
 
 const ProducersById = async (producers_id) => {
-
   const query = `SELECT producers.producers_name, producers.producers_id
   FROM producers
-  WHERE producers.producers_id = @producers_id`
-  return executeQuery(query, [{ name: 'producers_id', value: producers_id }]);
+  WHERE producers.producers_id = @producers_id`;
+  return executeQuery(query, [{ name: "producers_id", value: producers_id }]);
 };
 
 module.exports = {
