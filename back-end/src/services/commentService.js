afcf435fbf794053c21cd7969a35e073fe934c98
bachhungcb <bachhungcb@gmail.com>
2024-07-29 16:42:02 +0700

    clean code on commentService

--- back-end/src/services/commentService.js
+++ back-end/src/services/commentService.js
@@ -1,49 +1,7 @@
 const createPool = require(`../config/database`);
 const sql = require("mssql");
+const {executeProcedure, executeQuery} = require(`../config/procAndQueryConfig.js`);
 
-// Common function to execute queries
-const executeQuery = async (query, params) => {
-  try {
-    const pool = await createPool;
-    const request = pool.request();
-    for (const param of params) {
-      request.input(param.name, param.value);
-    }
-    const result = await request.query(query);
-    return result.recordset;
-  } catch (error) {
-    console.error("Database query error:", error);
-    throw new DatabaseQueryError(error, query);
-  }
-};
-
-//Common function to execute stored procedures
-const executeProcedure = async (procedure, params) => {
-  try {
-    const pool = await createPool;
-    const request = pool.request();
-    for (const param of params) {
-      request.input(param.name, param.value);
-    }
-    const result = await request.execute(procedure);
-    return { Result: 1, Recordset: result.recordset };
-  } catch (err) {
-    console.error("Database query error:", err);
-    if (err.number === 2627) {
-      // Primary Key Violation error number
-      return { Result: 0, Error: "Duplicate key error" };
-    }
-    return { Result: 0, Error: "Database query error" };
-  }
-};
-
-// Custom error class
-class DatabaseQueryError extends Error {
-  constructor(originalError, query) {
-    super(`Error executing query: ${query}`);
-    this.originalError = originalError;
-  }
-}
 
 //return 1 if can update user comment, 0 if cannot
 const updateUserComment = async (userId, animeId, comment) => {
