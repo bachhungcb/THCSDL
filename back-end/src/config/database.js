4937551a1bf2e5ede181fdab9d506f5da1fe5b29
bachhungcb <bachhungcb@gmail.com>
2024-07-29 16:41:13 +0700

    .

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -28,43 +28,5 @@ async function createPool() {
   }
 }
 
-const executeQuery = async (query, params) => {
-  try {
-    const pool = await createPool;
-    const request = pool.request();
-    for (const param of params) {
-      request.input(param.name, param.value);
-    }
-    const result = await request.query(query);
-    return result.recordset;
-  } catch (error) {
-    console.error("Database query error:", error);
-    throw new DatabaseQueryError(error, query);
-  }
-};
-
-//Common function to execute stored procedures
-const executeProcedure = async (procedure, params) => {
-  try {
-    const pool = await createPool;
-    const request = pool.request();
-    for (const param of params) {
-      request.input(param.name, param.value);
-    }
-    const result = await request.execute(procedure);
-    return result.recordset;
-  } catch (err) {
-    console.error("Database query error:", err);
-    throw new DatabaseQueryError(err, procedure);
-  }
-};
-
-// Custom error class
-class DatabaseQueryError extends Error {
-  constructor(originalError, query) {
-    super(`Error executing query: ${query}`);
-    this.originalError = originalError;
-  }
-}
 //export config to use in other files
 module.exports = createPool();
