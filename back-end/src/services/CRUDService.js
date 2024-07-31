c0ad36e5d87e44f2f62abe353900c824ffdfc4ce
bachhungcb <bachhungcb@gmail.com>
2024-07-31 09:12:12 +0700

    clean code for comment services

--- back-end/src/services/CRUDService.js
+++ back-end/src/services/CRUDService.js
@@ -1,73 +1,57 @@
 const sql = require("mssql");
 const sqlConfig = require("../config/database");
 const createPool = require(`../config/database`);
+const {executeProcedure, executeQuery} = require(`../config/procAndQueryConfig.js`);
 
-const executeProcedure = async (procedure, params) => {
-  try {
-    const pool = await createPool;
-    const request = pool.request();
-    for (const param of params) {
-      request.input(param.name, param.value);
-    }
-    const result = await request.execute(procedure);
-    return { Result: 1, Recordset: result.recordset };
-  } catch (err) {
-    console.error("Database query error:", err);
-    if (err.number === 2627) {
-      // Primary Key Violation error number
-      return { Result: 0, Error: "Duplicate key error" };
-    }
-    return { Result: 0, Error: "Database query error" };
-  }
-};
 
 const getComments = async (animeId) => {
-  try {
-    let pool = await sql.connect(sqlConfig);
-    let result = await pool.request().input("animeId", sql.Int, animeId)
-      .query(`SELECT c.*, u.FullName, u.Avatar FROM User_comment c 
-                    JOIN Users u ON u.Id = c.users_id
-                    WHERE c.anime_id = @animeId`);
+  // try {
+  //   let pool = await sql.connect(sqlConfig);
+  //   let result = await pool.request().input("animeId", sql.Int, animeId)
+  //     .query(`SELECT c.*, u.FullName, u.Avatar FROM User_comment c 
+  //                   JOIN Users u ON u.Id = c.users_id
+  //                   WHERE c.anime_id = @animeId`);
 
-    return result.recordset;
-  } catch (err) {
+  //   return result.recordset;
+  // } catch (err) {
+  //   console.log(err);
+  //   throw err;
+  // }
+  try{
+    const query =     `SELECT c.*, u.FullName, u.Avatar FROM User_comment c 
+                      JOIN Users u ON u.Id = c.users_id
+                      WHERE c.anime_id = @animeId`;
+    return executeQuery(query, [{ name: "animeId", value: animeId }]);
+  }catch(err){
     console.log(err);
     throw err;
   }
+
 };
 
 const postComment = async (userId, animeId, comment) => {
   try {
-    let pool = await sql.connect(sqlConfig);
-
-    const userExists = await pool
-      .request()
-      .input("userId", sql.Int, userId)
-      .query("SELECT Id FROM Users WHERE Id = @userId");
-
-    if (userExists.recordset.length === 0) {
+    
+    const userExists = await executeQuery(`SELECT Id FROM Users WHERE Id = @userId`,
+                                     [{ name: "userId", value: userId }]);
+    console.log(userExists.length);
+    if (userExists.length === 0) {
       throw new Error(`User with ID ${userId} does not exist.`);
     }
 
-    const animeExists = await pool
-      .request()
-      .input("animeId", sql.Int, animeId)
-      .query("SELECT anime_id FROM anime WHERE anime_id = @animeId");
-
-    if (animeExists.recordset.length === 0) {
+    const animeExists = await executeQuery(`SELECT anime_id FROM anime WHERE anime_id = @animeId`, 
+                                      [{ name: "animeId", value: animeId }]);
+    if (animeExists.length === 0) {
       throw new Error(`Anime with ID ${animeId} does not exist.`);
     }
-
-    // Proceed with inserting comment
-    let result = await pool
-      .request()
-      .input("userId", sql.Int, userId)
-      .input("animeId", sql.Int, animeId)
-      .input("comment", sql.NVarChar, comment)
-      .input("addedAt", sql.Date, new Date())
-      .query(`INSERT INTO User_comment (users_id, anime_id, comment, added_at) 
-                    VALUES (@userId, @animeId, @comment, @addedAt)`);
-
+    
+    let result = await executeQuery(` INSERT INTO User_comment (users_id, anime_id, comment, added_at) 
+                                      VALUES (@userId, @animeId, @comment, @addedAt)`, 
+                              [ {name: "userId", value: userId}, 
+                                {name: "animeId", value: animeId}, 
+                                {name: "comment", value: comment}, 
+                                {name: "addedAt", value: new Date()}]);
+    console.log(result);
     return result;
   } catch (err) {
     console.log(err);
